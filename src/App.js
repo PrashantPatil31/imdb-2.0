3378c89e80243c7c481f307e30bf7ba5115f6428
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2022-11-26 13:26:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import FanFavorites from "./components/FanFavorites";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="  ">
+      {/* Navbar */}
+      <Navbar />
+      {/* Hero */}
+      <Hero />
+      {/* Fan Favorites */}
+      <FanFavorites />
+      {/* Exclusive Videos */}
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
